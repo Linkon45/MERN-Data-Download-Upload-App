a15a1dfd766a58e2168b8f5067dcdc6fb1bee9c6
Linkon45 <linkon3.1416@gmail.com>
2022-07-12 04:30:44 +0600

    Add user authentication unit-testing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,7 +56,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email })
 
   if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
+    res.status(200).json({
       _id: user.id,
       name: user.name,
       email: user.email,
